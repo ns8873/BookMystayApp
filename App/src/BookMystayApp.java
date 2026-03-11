76f5a2d90cf4d716913dc0f0b864b56ba0f63d9f
ns8873 <ns8873@srmist.edu.in>
2026-03-11 09:18:36 +0530

    naveen Added UC1-WelcomePage

---
+++ App/src/BookMystayApp.java
@@ -0,0 +1,8 @@
+public class BookMystayApp {
+    public static void main(String[] args){
+
+        System.out.println("Welcome To Hotel Booking Management System");
+        System.out.println("Version 1.0");
+        System.out.println("System Initialized successfully");
+    }
+}
